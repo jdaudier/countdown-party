7810e1af44fd5a0c0855da9347d96e2840dc2d28
Joanne Daudier <jdaudier@outlook.com>
2014-03-01 17:44:36 -0500

    Homepage design done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,40 @@
 $(document).ready(function(){
-    $('.datepicker').pickadate();
+    $('.datepicker').pickadate({
+        format: 'mmm dd, yyyy',
+        formatSubmit: 'mm/dd/yyyy'
+    });
+
     $('.timepicker').pickatime();
 
 
+    $('.ui.form')
+        .form({
+            eventName: {
+                identifier  : 'eventname',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Please enter your event name'
+                    }
+                ]
+            },
+            datePicker: {
+                identifier  : 'datepicker',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Please pick a date'
+                    }
+                ]
+            },
+            timePicker: {
+                identifier : 'timepicker',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Please pick a time'
+                    }
+                ]
+            }
+        });
 });
