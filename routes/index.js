5018b0851a2116e135f329153f3aba608f906a49
Joanne Daudier <jdaudier@outlook.com>
2014-03-04 22:14:25 -0500

    Update footer

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Countdown timer to your event' });
+  res.render('index', { title: 'Countdown timer to your event | Countdown Party' });
 };
