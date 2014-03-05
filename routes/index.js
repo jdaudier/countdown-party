59a5e7418df94f68374f8929d9819e2f9df456ba
Joanne Daudier <jdaudier@outlook.com>
2014-03-04 22:12:07 -0500

    Update title :grinning:

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Countdown Party: Countdown timer to your event' });
+  res.render('index', { title: 'Countdown timer to your event' });
 };
